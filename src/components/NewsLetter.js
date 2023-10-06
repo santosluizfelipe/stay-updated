faa314abb03c8b7c6899e009ece8346da28a1790
Luiz Felipe <luizfelipe@Luizs-MacBook-Air.local>
2023-10-06 15:01:03 +0100

    wip

---
+++ src/components/NewsLetter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './NewsLetter.css';
+
+const NewsLetter = () => {
+  return(
+    <div className="form-container">hello world</div>
+  )
+}
+
+export default NewsLetter
